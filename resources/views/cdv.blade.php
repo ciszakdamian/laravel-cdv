0f34e1c2dda1681aa0e4ba8e028d6882d764dc77
ciszakdamian <ciszakdamian@gmail.com>
2020-04-25 09:40:35 +0200

    test

--- resources/views/cdv.blade.php
+++ resources/views/cdv.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    CDV
+                    Welcome CDV
                 </div>
 
            </div>
