e5c12e671d28cd3157773c9f50dad16afb5b55e7
ciszakdamian <ciszakdamian@gmail.com>
2020-04-26 09:09:45 +0200

    test 2

--- resources/views/loop.blade.php
+++ resources/views/loop.blade.php
@@ -99,7 +99,7 @@
                 <tr>
                 @php
                  for($i=0;$i<count($car);$i++){
-                        echo "<td>".$car[$i]."</td>";
+                        print "<td>".$car[$i]."</td>";
 
                  }
 
